cba8b62b59990930b2be42cc078f81f19b959a25
Dzhuzeppe108 <nikita.e.gavrilov@gmail.com>
2019-09-30 19:22:06 +0500

    Попытка сдать ДЗ №6

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "homework-1.h"
-
-int main(int argc, const char** argv)
-{
-    puts("HOMEWORK\n");
-
-    massIndex(1, 1);
-    puts("");
-    exchangeValues(1, 1);
-    puts("");
-    years(1);
-    puts("");
-    // chess(1, 1, 1, 1); НЕ РАБОТАЕТ
-    puts("");
-    tenthQuest(1);
-
-    return 0;
-}
+/* #include <stdio.h>
+*  #include <stdlib.h>
+*  #include <string.h>
+*  #include "homework-1.h"
+*
+*  int main(int argc, const char** argv)
+*  {
+*      puts("HOMEWORK\n");
+*
+*      massIndex(1, 1);
+*      puts("");
+*      exchangeValues(1, 1);
+*      puts("");
+*      years(1);
+*      puts("");
+*      // chess(1, 1, 1, 1); НЕ РАБОТАЕТ
+*      puts("");
+*      tenthQuest(1);
+*
+*      return 0;
+*  }
+*/
